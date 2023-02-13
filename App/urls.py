b4b8a1e1ec63fbe91c7c1980d5539827fde65172
X551826 <geovannycs@prefeitura.sp.gov.br>
2023-02-13 10:30:45 -0300

    template_static-root

--- App/urls.py
+++ App/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.nome_da_função, name='nome da funcao'),
-    path('cadastro/',views.cadastro,name='cadastro')
+    path('cadastro/',views.cadastro,name='cadastro'),
+    path('Info/',views.info,name='info')
 ]
--- App/views.py
+++ App/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 def nome_da_função(request):
 	return render(request, "pages/index.html")
+
 def cadastro(request):
 	return render(request,"pages/cadastro.html")
+
+def info(request):
+	return render(request, "pages/info.html")
+
+# def login(request):
+# 	return render(request, "pages/login.html")
