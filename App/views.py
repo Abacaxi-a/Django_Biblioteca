2d34e62c7d0a246c491fa254c3fb7478ff4a0007
X551826 <geovannycs@prefeitura.sp.gov.br>
2023-02-24 15:52:36 -0300

    blocos

--- App/views.py
+++ App/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def nome_da_função(request):
-	return render(request, "pages/index.html")
+	return render(request, "pages/home.html")
 
 def cadastro(request):
 	return render(request,"pages/cadastro.html")
